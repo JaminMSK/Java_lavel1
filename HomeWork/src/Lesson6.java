89cfe1984186f8e91881036c3102ef9729b41240
JaminMSK <Lagils@mail.ru>
2022-03-01 16:56:22 +0300

    ДЗ6 переделанно

--- HomeWork/src/Lesson6.java
+++ HomeWork/src/Lesson6.java
@@ -1,56 +1,84 @@
+/* Сделал Vladislav Torgashev
+01.03.2022
+ */
 public class Lesson6 {
-    public static void main(String[] args) {
-        Animal[] animals = {
-                new Cat("Murzik"),
-                new Dog("Bobik")
-        };
-        for (Animal animal : animals) {
-            System.out.println(animal);
-        }
-    }
-}
 
-class Cat extends Animal {
-    Cat(String name) {
-        super(name);
-    }
-    @Override
-    public  int run() {
-        return  200;
-    }
-    @Override
-    public  String swim() {
-        return "кот не умеет плавать";
+    public static void main(String[] args) {
+        Cat cat = new Cat(200);
+        Dog dog = new Dog(500, 10);
+
+        IAnimal [] animals = {cat, dog};
+        for (IAnimal animal : animals) {
+            System.out.println(animal);
+            System.out.println(animal.run(150));
+            System.out.println(animal.run(250));
+            System.out.println(animal.run(550));
+            System.out.println(animal.swim(5));
+            System.out.println(animal.swim(15));
+        }
+        System.out.println("Animals were created: " + Animal.getCountOfAnimals());
     }
 }
 
 class Dog extends Animal {
-    Dog(String name) {
-        super(name);
-    }
-    @Override
-    public  int run() {
-        return  500;
-    }
-    @Override
-    public  String swim() {
-        return  "10м.";
+    Dog (int runLimit, int swimLimit){
+        super(runLimit, swimLimit);
     }
 }
 
-interface IAnimal {
-    int run();
-    String swim();
+class Cat extends Animal {
+    Cat(int runLimit){
+        super(runLimit, -1);
+    }
+    @Override
+    public  String swim(int distance){
+        return getClassName() + " can't swim";
+    }
 }
 
  abstract class Animal implements IAnimal{
-    protected String name;
+    protected int runLimit;
+    protected int swimLimit;
+    protected String className;
+    protected static int countOfAnimals = 0;
 
-    Animal(String name){
-        this.name = name;
+    Animal(int runLimit, int swimLimit){
+        this.runLimit = runLimit;
+        this.swimLimit = swimLimit;
+        className = getClass().getSimpleName();
+        countOfAnimals++;
     }
-     @Override
-     public  String toString() {
-         return name + " " + "может пройти: " + run() + "м. и " + " может проплыть: " + swim();
+
+     public String getClassName() {
+         return className;
      }
+     public static int getCountOfAnimals(){
+        return countOfAnimals;
+     }
+
+     @Override
+     public String run(int distance){
+        if (distance > runLimit) {
+            return  className + " couldn't run " + distance;
+        } else {
+            return className + "successfully run " + distance;
+        }
+     }
+     @Override
+     public String swim(int distance){
+         if (distance > swimLimit) {
+             return  className + " couldn't swim " + distance;
+         } else {
+             return className + "successfully swim " + distance;
+         }
+     }
+     @Override
+     public String toString() {
+        return  className + " runLimit: " + runLimit + ", swimLimit: " + swimLimit;
+     }
+ }
+
+interface IAnimal {
+    String run(int distance);
+    String swim(int distance);
 }
